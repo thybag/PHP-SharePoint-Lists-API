be4c63f2511d9a7bc9cf2aeea9d819e67bfe5fac
thybag <thybag@gmail.com>
2014-01-23 19:01:26 +0000

    lib -> src

--- SharePointAPI.php
+++ SharePointAPI.php
@@ -19,7 +19,7 @@
 spl_autoload_register(function ($classname) {
 	$classname = ltrim($classname, "\\");
 	preg_match('/^(.+)?([^\\\\]+)$/U', $classname, $match);
-	$classname = 'lib/'.str_replace("\\", "/", $match[1])
+	$classname = 'src/'.str_replace("\\", "/", $match[1])
 		. str_replace(array("\\", "_"), "/", $match[2])
 		. ".php";
 	include_once $classname;
